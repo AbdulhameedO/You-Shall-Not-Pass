cc4ef444423e22494fcb6a500c2a481b12cdd86e
ElmeSelhyy <119946754+ElmeSelhyy@users.noreply.github.com>
2023-12-08 22:29:11 +0200

    colors and plates datasets
    Colors and plates datasets

--- processing.py
+++
@@ -1,9 +0,0 @@
-# This file is for the post-processing of the image.
-# This contains the main algorithms for handling the image.
-
-# Function: post_processing
-
-def processing(image):
-    
-    # Return the image
-    return image
